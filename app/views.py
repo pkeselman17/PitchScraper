5874ff7cb83901c08be3c371122e9bc9b1b9df58
Phil Keselman <pkeselman17@gmail.com>
2015-07-24 09:02:53 -0400

    Modified template to include model

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Sport
 
 def sport_list(request):
-	return render(request, 'sport/sport_list.html', {})
+	sports = Sport.objects.order_by('name')
+	return render(request, 'sport/sport_list.html', {'sports': sports})
