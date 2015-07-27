7b741d3591e92ab82f3f339bc92adb819f4f4154
Phil Keselman <pkeselman17@gmail.com>
2015-07-27 15:23:07 -0400

    Added content to home page and styles

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import Permission, User
+from app.models import Sport
 
 def index(request):
 	if request.user.is_authenticated():
-		return render(request, 'home/index.html', {})
+		return render(request, 'home/index.html', {"sports":Sport.objects.all()})
 	return redirect('accounts/login/')
